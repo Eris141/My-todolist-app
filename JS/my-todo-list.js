41c744b8235ca79fedd12ef326742d53939f340f
Eris141 <etogod77@gmail.com>
2024-07-28 14:37:58 +0200

    Css fixed Ver2

--- JS/my-todo-list.js
+++ JS/my-todo-list.js
@@ -1,1 +1,118 @@
+
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+
+//function to save the tasks to local storage
+function saveTaskToLocalStoreage() {
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+//function to load tasks from local storage
+function loadTasksFromLocalStorage() {
+  const storedTasks = localStorage.getItem('tasks');
+  if (storedTasks) {
+    tasks = JSON.parse(storedTasks);
+  }
+}
+
+//function to add a new task
+
+function addTask(taskText) {
+  const newTask = {
+    text: taskText,
+    completed: false
+  };
+  tasks.push(newTask);
+  saveTaskToLocalStoreage();
+}
+//get references to the input field and rask list
+const newTodoInput = document.getElementById('input-todo');
+const taskList = document.getElementById('task-list');
+
+// add the event listtner for the button action
+const addTodoButton = document.getElementById('add-todo-button');
+
+//handleAddTodo is the fucntion that makes the process when the butt is clickd
+document.addEventListener("DOMContentLoaded", function() {
+  addTodoButton.addEventListener('click', handleAddTodo);
+  newTodoInput.addEventListener('keydown', function (event) {
+    if(event.key === "Enter") {
+      event.preventDefault();
+      handleAddTodo();
+    }
+  });
+});
+
+//here is the fucntion that adds the new task when button is cliked
+function handleAddTodo() {
+  //get the value that is entered by user
+  const getNewTaskText = newTodoInput.value.trim();
+
+  //check if the user has entered text or not 
+  if (getNewTaskText) {
+    addTodoItem(getNewTaskText);
+
+     //clear the input field for the next task
+    newTodoInput.value = '';
+  } else {
+    alert("Please enter a task!");
+  }
+
+}
+
+function addTodoItem(getNewTaskText) {
+
   
+  //create a new list item for the task
+  const newTodoItem = document.createElement('li');
+  newTodoItem.className = "task-item";
+
+  //div inside the li
+  const divInsideLi = document.createElement('div');
+  divInsideLi.classList.add('todotask-div');
+  //creates a new variable to store the input with the
+  const checkbox = document.createElement('input');
+  checkbox.type = 'checkbox';
+  checkbox.className = 'task-checkbox';
+
+  checkbox.addEventListener('click', completedTodo);
+
+  const span = document.createElement('span');
+  span.className = 'task-text';
+  span.textContent = getNewTaskText;
+
+
+  //add checbox and test to div
+  divInsideLi.appendChild(checkbox);
+  divInsideLi.appendChild(span);
+  //add div to li
+  newTodoItem.appendChild(divInsideLi);
+  //add all to task list
+  taskList.appendChild(newTodoItem);
+
+  //fucntion adds delete button and deletes the task when clicked
+  deleteTodoButon(newTodoItem);
+}
+
+//add delete button
+function deleteTodoButon(newTodoItem) {
+  // creates the delete button on the task
+  const deleteButton = document.createElement('button');
+  deleteButton.textContent = 'Delete';
+  //add the event listner when clicket so i can delete the task
+  deleteButton.addEventListener('click', handleDeleteTask);
+
+  newTodoItem.appendChild(deleteButton);
+}
+
+//removes todo list  when click button 
+function handleDeleteTask(event) {
+  //removes the task from the list
+  const taskItem = event.target.parentNode;
+  taskList.removeChild(taskItem);
+}
+
+function completedTodo(event) {
+  const item = event.target.parentElement;
+  item.classList.toggle('completed');
+}
